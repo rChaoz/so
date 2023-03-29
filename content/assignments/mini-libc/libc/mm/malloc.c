580360ffb372a13d9d4941ffdadf67579a8eff75
rChaoz <catalin_trand2@yahoo.com>
2023-03-29 11:43:53 +0300

    Implement memory functions

--- content/assignments/mini-libc/libc/mm/malloc.c
+++ content/assignments/mini-libc/libc/mm/malloc.c
@@ -7,31 +7,38 @@
 #include <string.h>
 #include <stdlib.h>
 
-void *malloc(size_t size)
-{
-	/* TODO: Implement malloc(). */
-	return NULL;
+void *malloc(size_t size) {
+	void *p = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (p == MAP_FAILED) return NULL;
+    if (mem_list_add(p, size) < 0) {
+        munmap(p, size);
+        return NULL;
+    }
+    return p;
 }
 
-void *calloc(size_t nmemb, size_t size)
-{
-	/* TODO: Implement calloc(). */
-	return NULL;
+void *calloc(size_t nmemb, size_t size) {
+    // Becuase malloc uses mmap with MAP_ANONYMOUS, which initializes memory with zeroes
+	return malloc(nmemb * size);
 }
 
-void free(void *ptr)
-{
-	/* TODO: Implement free(). */
+void free(void *ptr) {
+    struct mem_list *item = mem_list_find(ptr);
+    if (item == NULL) return;
+    if (munmap(item->start, item->len) < 0) return;
+    mem_list_del(ptr);
 }
 
-void *realloc(void *ptr, size_t size)
-{
-	/* TODO: Implement realloc(). */
-	return NULL;
+void *realloc(void *ptr, size_t size) {
+    struct mem_list *item = mem_list_find(ptr);
+    if (item == NULL) return NULL;
+    void *p = mremap(item->start, item->len, size, MREMAP_MAYMOVE);
+    if (p == MAP_FAILED) return NULL;
+    item->start = p;
+    item->len = size;
+	return p;
 }
 
-void *reallocarray(void *ptr, size_t nmemb, size_t size)
-{
-	/* TODO: Implement reallocarray(). */
-	return NULL;
+void *reallocarray(void *ptr, size_t nmemb, size_t size) {
+	return realloc(ptr, nmemb * size);
 }
--- content/assignments/mini-libc/libc/mm/mmap.c
+++ content/assignments/mini-libc/libc/mm/mmap.c
@@ -4,20 +4,22 @@
 #include <errno.h>
 #include <internal/syscall.h>
 
-void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
-{
-	/* TODO: Implement mmap(). */
-	return MAP_FAILED;
+void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) {
+    long ret = syscall(__NR_mmap, addr, length, prot, flags, fd, offset);
+    if (ret < 0) {
+        errno = (int) -ret;
+        return MAP_FAILED;
+    } else return (void*) ret;
 }
 
-void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
-{
-	/* TODO: Implement mremap(). */
-	return MAP_FAILED;
+void *mremap(void *old_address, size_t old_size, size_t new_size, int flags) {
+    long ret = syscall(__NR_mremap, old_address, old_size, new_size, flags);
+    if (ret < 0) {
+        errno = (int) -ret;
+        return MAP_FAILED;
+    } else return (void *) ret;
 }
 
-int munmap(void *addr, size_t length)
-{
-	/* TODO: Implement munmap(). */
-	return -1;
+int munmap(void *addr, size_t length) {
+    return syscall_errhandle(__NR_munmap, addr, length);
 }
