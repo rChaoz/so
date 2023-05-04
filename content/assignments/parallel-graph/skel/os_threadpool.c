9277f4c57e507658f531689488178a6e010292e9
rChaoz <catalin_trand2@yahoo.com>
2023-05-04 14:01:37 +0300

    assign3: Add max_tasks option to threadpool

--- content/assignments/parallel-graph/skel/os_threadpool.c
+++ content/assignments/parallel-graph/skel/os_threadpool.c
@@ -14,13 +14,23 @@ os_task_t *task_create(void *arg, void (*f)(void *)) {
 }
 
 /* Add a new task to threadpool task queue */
-void add_task_in_queue(os_threadpool_t *tp, os_task_t *t) {
+int add_task_in_queue(os_threadpool_t *tp, os_task_t *t) {
+    if (tp->num_tasks == tp->max_tasks) return 0; // quick check
     pthread_mutex_lock(&tp->taskLock);
+    if (tp->num_tasks == tp->max_tasks) {
+        // ensure we really don't add too many tasks
+        pthread_mutex_unlock(&tp->taskLock);
+        return 0;
+    }
+
     os_task_queue_t *task = malloc(sizeof(os_task_queue_t));
     task->task = t;
     task->next = tp->tasks;
     tp->tasks = task;
+    ++tp->num_tasks;
+
     pthread_mutex_unlock(&tp->taskLock);
+    return 1;
 }
 
 /* Get the head of task queue from threadpool */
@@ -28,6 +38,7 @@ os_task_t *get_task(os_threadpool_t *tp) {
     if (tp->tasks == NULL) return NULL;
     os_task_queue_t *task = tp->tasks;
     tp->tasks = task->next;
+    --tp->num_tasks;
     os_task_t *t = task->task;
     free(task);
     return t;
@@ -36,8 +47,9 @@ os_task_t *get_task(os_threadpool_t *tp) {
 /* === THREAD POOL === */
 
 /* Initialize the new threadpool */
-os_threadpool_t *threadpool_create(unsigned int nThreads) {
+os_threadpool_t *threadpool_create(unsigned int nTasks, unsigned int nThreads) {
     os_threadpool_t *pool = calloc(1, sizeof(os_threadpool_t));
+    pool->max_tasks = nTasks;
     pool->num_threads = nThreads;
     pthread_mutex_init(&pool->taskLock, NULL);
 
@@ -54,7 +66,8 @@ os_threadpool_t *threadpool_create(unsigned int nThreads) {
     return pool;
 }
 
-struct timespec tenMillis = { 0, 10000000L };
+// One millisecond
+struct timespec milli = {0, 1000000L};
 
 /* Loop function for threads */
 void *thread_loop_function(void *args) {
@@ -65,7 +78,7 @@ void *thread_loop_function(void *args) {
         os_task_t *task = get_task(tp);
         pthread_mutex_unlock(&tp->taskLock);
         // If there is no task, wait 10 millis before trying to grab task again
-        if (task == NULL) nanosleep(&tenMillis, NULL);
+        if (task == NULL) nanosleep(&milli, NULL);
         else {
             // Run task function
             task->task(task->argument);
@@ -78,7 +91,7 @@ void *thread_loop_function(void *args) {
 void threadpool_stop(os_threadpool_t *tp, int (*processingIsDone)(os_threadpool_t *)) {
     // Wait for processing to complete
     if (processingIsDone != NULL) {
-        while (!processingIsDone(tp)) nanosleep(&tenMillis, NULL);
+        while (!processingIsDone(tp)) nanosleep(&milli, NULL);
     }
     // Call for stop and wait for all threads to finish
     tp->should_stop = 1;
