a380926ad123f30528f7c331855045c6e63bddd8
rChaoz <catalin_trand2@yahoo.com>
2023-05-04 14:48:18 +0300

    assign3: Add vislock, fix issues, 100/100

--- content/assignments/parallel-graph/skel/parallel.c
+++ content/assignments/parallel-graph/skel/parallel.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <time.h>
 
 #include "os_graph.h"
 #include "os_threadpool.h"
@@ -11,49 +10,58 @@
 #define MAX_THREAD 4
 
 pthread_mutex_t sumLock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t visLock = PTHREAD_MUTEX_INITIALIZER;
+
 os_threadpool_t *tp;
-int sum = 0;
+
 os_graph_t *graph;
+volatile int sum = 0;
 
 // Function to add a task to threadpool, trying again if tasklist is full
-struct timespec milli = {0, 1000000L};
 void processNode(void *nodeIdxVoid);
 void add_node_task(unsigned int nodeIdx) {
-    os_task_t *task = task_create((void*) (long long) nodeIdx, processNode);
-    while (!add_task_in_queue(tp, task)) nanosleep(&milli, NULL);
+    os_task_t *task = task_create((void*) (long) nodeIdx, processNode);
+    add_task_in_queue(tp, task);
 }
 
 void processNode(void *nodeIdxVoid) {
-    unsigned int nodeIdx = (unsigned int) (long long) nodeIdxVoid;
-    // It is possible to add a node twice ocasionally, still, don't process it twice
-    // We prefer to do this instead of use a lock because a lock would be significantly slower
-    if (graph->visited[nodeIdx]) return;
-
+    unsigned int nodeIdx = (unsigned int) (long) nodeIdxVoid;
     os_node_t *node = graph->nodes[nodeIdx];
 
     pthread_mutex_lock(&sumLock);
     sum += node->nodeInfo;
     pthread_mutex_unlock(&sumLock);
 
-    for (int i = 0; i < node->cNeighbours; i++)
+    // Add neighbours to task queue
+    for (int i = 0; i < node->cNeighbours; i++) {
+        pthread_mutex_lock(&visLock);
         if (graph->visited[node->neighbours[i]] == 0) {
             graph->visited[node->neighbours[i]] = 1;
             add_node_task(node->neighbours[i]);
         }
-}
-
-void traverse_graph() {
-    for (int i = 0; i < graph->nCount; i++) {
-        if (graph->visited[i] == 0) {
-            graph->visited[i] = 1;
-            add_node_task(i);
-        }
+        pthread_mutex_unlock(&visLock);
     }
+    // Node process complete
+    graph->visited[nodeIdx] = 2;
 }
 
 // Check if entire graph was processsed
 int is_done() {
-    for (int i = 0; i < graph->nCount; ++i) if (!graph->visited[i]) return 0;
+    for (int i = 0; i < graph->nCount; ++i) {
+        // If the graph is not connected, add the first unprocessed node to the task queue
+        // when all tasks are complete
+        if (graph->visited[i] == 0) {
+            if (tp->tasks == NULL) {
+                pthread_mutex_lock(&visLock);
+                if (graph->visited[i] == 0) {
+                    graph->visited[i] = 1;
+                    add_node_task(i);
+                }
+                pthread_mutex_unlock(&visLock);
+            }
+            return 0;
+        } else if (graph->visited[i] != 2) return 0;
+    }
     return 1;
 }
 
@@ -77,7 +85,7 @@ int main(int argc, char *argv[]) {
     }
 
     tp = threadpool_create(MAX_TASK, MAX_THREAD);
-    traverse_graph();
+    // is_done will add the first node to queue
     threadpool_stop(tp, is_done);
 
     printf("%d", sum);
