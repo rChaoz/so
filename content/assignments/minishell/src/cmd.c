6d2d7b95f8e0d8b11f4c9c29947cc24bb0c408c6
rChaoz <catalin_trand2@yahoo.com>
2023-05-16 21:57:12 +0300

    assign4: Add environment variable support

--- content/assignments/minishell/src/cmd.c
+++ content/assignments/minishell/src/cmd.c
@@ -14,14 +14,23 @@
 #include "cmd.h"
 #include "utils.h"
 
-#define READ		0
-#define WRITE		1
+#define READ        0
+#define WRITE       1
 
+/**
+ * Get an environment variable, or the empty string "", if not found.
+ */
+static const char* get_env(const char *name) {
+    const char *value = getenv(name);
+    if (value == NULL) return "";
+    else return value;
+}
 
 /**
  * Parse a word, expanding variables as necessary
  */
-static char* parse_word(word_t *word) {
+static char *parse_word(word_t *word)
+{
     char *res = malloc(1024);
     res[0] = '\0';
     size_t bufLength = 0, length = 0;
@@ -29,7 +38,8 @@ static char* parse_word(word_t *word) {
     while (word != NULL) {
         const char *toAdd = word->string;
 
-        if (word->expand) { /* TODO */ }
+        if (word->expand)
+            toAdd = get_env(toAdd);
 
         size_t toAddLength = strlen(toAdd);
         if (length + toAddLength >= bufLength) {
@@ -45,52 +55,66 @@ static char* parse_word(word_t *word) {
     return res;
 }
 
-const word_t HOME_WORD = { "HOME", true, NULL, NULL };
-
 /**
  * Internal show or change-directory command.
  */
-static int shell_cd(word_t *dir, FILE *out, FILE *err)
+static int shell_cd(word_t *dir, FILE *err)
 {
-    if (dir == &HOME_WORD) {} // TODO check if $HOME is set, show error otherwise
+    // First, get current working directory
+    char oldDir[1024];
+    int saveOldDir = getcwd(oldDir, 1024) != NULL;
 
-    char *path = parse_word(dir);
+    int res;
 
-    if (strcmp(path, "-") == 0) {
-        // TODO check if $OLDPWD is set and change dir to that
+    // If no argument is provided, change directory to $HOME
+    if (dir == NULL) {
+        const char *home = getenv("HOME");
+        if (home == NULL) {
+            fputs("The $HOME variable is not set.\n", err);
+            return EXIT_FAILURE;
+        } else res = chdir(home);
+
+    } else {
+        // If the argument is "-", change directory to $OLDPWD
+        char *path = parse_word(dir);
+        if (strcmp(path, "-") == 0) {
+            const char *old = getenv("OLDPWD");
+            if (old == NULL) {
+                fputs("The $OLDPWD variable is not set.\n", err);
+                return EXIT_FAILURE;
+            } else res = chdir(old);
+        } else res = chdir(path);
+        free(path);
     }
 
-    // First, save current working directory into $OLDPWD
-    char buf[1024];
-    // If pwd fails, fail silently (don't change $OLDPWD)
-    if (getcwd(buf, 1024) != NULL) {
-        // TODO set $OLDPWD to BUF
-    }
-
-    int res = chdir(path);
-
-    free(path);
+    // Print error (if any) and return
     if (res != 0) {
         switch (errno) {
             case ENOENT:
-                fprintf(err, "The specified path cannot be found.\n");
+                fputs("The specified path cannot be found.\n", err);
                 break;
             case EACCES:
-                fprintf(err, "Permission denied.\n");
+                fputs("Permission denied.\n", err);
                 break;
             default:
-                fprintf(err, "Unknown error.\n");
+                fputs("Unknown error.\n", err);
                 break;
         }
         return errno;
-    }
-	return 0;
+
+        // If directory was changed successfully, save old dir into $OLDPWD
+        // If pwd failed, fail silently (don't change $OLDPWD)
+    } else if (saveOldDir)
+        setenv("OLDPWD", oldDir, 1);
+
+    return 0;
 }
 
 /**
  * Internal show working-directory command
  */
-static int shell_pwd(FILE *out, FILE *err) {
+static int shell_pwd(FILE *out, FILE *err)
+{
     char buf[1024];
     if (getcwd(buf, 1024) == NULL) {
         fprintf(err, "Unable to get working directory: possibly path too long\n");
@@ -105,11 +129,13 @@ static int shell_pwd(FILE *out, FILE *err) {
 /**
  * Internal exit/quit command.
  */
-static int shell_exit(void) {
-	return SHELL_EXIT;
+static int shell_exit(void)
+{
+    return SHELL_EXIT;
 }
 
-static FILE* open_file(word_t *word, char *mode) {
+static FILE *open_file(word_t *word, char *mode)
+{
     char *fileName = parse_word(word);
     FILE *f = fopen(fileName, mode);
     if (f == NULL) {
@@ -127,7 +153,34 @@ static FILE* open_file(word_t *word, char *mode) {
  */
 static int parse_simple(simple_command_t *s, int level, command_t *father)
 {
-	/* TODO: Sanity checks. */
+    /* TODO: Sanity checks. */
+
+    // Check if variable assignment
+    const word_t *eq = s->verb->next_part;
+    if (eq != NULL && strcmp(eq->string, "=") == 0) {
+        const char *varName = s->verb->string;
+
+        if (eq->next_part == NULL) {
+            // Assigning nothing, e.g. "NAME="
+            // Delete the variable
+            if (s->verb->expand) {
+                fprintf(stderr, "Variable name may start with dollar sign.");
+                return EXIT_FAILURE;
+            }
+            unsetenv(varName);
+        } else {
+            // Assigning value
+            const char *value = eq->next_part->string;
+
+            // Expand variable if necessary
+            if (eq->next_part->expand)
+                value = get_env(value);
+
+            setenv(varName, value, 1);
+        }
+
+        return 0;
+    }
 
     // Resolve input/outputs redirections
     FILE *in = stdin;
@@ -157,16 +210,12 @@ static int parse_simple(simple_command_t *s, int level, command_t *father)
     char *command = parse_word(s->verb);
     int ret = 0;
     if (strcmp(command, "cd") == 0)
-        ret = shell_cd(s->params, out, err);
+        ret = shell_cd(s->params, err);
     else if (strcmp(command, "pwd") == 0)
         ret = shell_pwd(out, err);
     else if (strcmp(command, "exit") == 0 || strcmp(command, "quit") == 0)
         ret = shell_exit();
-    else if (1) {
-        /* TODO: If variable assignment, execute the assignment and return
-         * the exit status.
-         */
-    } else if (2) {
+    else if (2) {
         /* TODO: If external command:
          *   1. Fork new process
          *     2c. Perform redirections in child
@@ -182,29 +231,29 @@ static int parse_simple(simple_command_t *s, int level, command_t *father)
     if (err != stderr) fclose(err);
     free(command);
 
-	return ret;
+    return ret;
 }
 
 /**
  * Process two commands in parallel, by creating two children.
  */
 static int run_in_parallel(command_t *cmd1, command_t *cmd2, int level,
-		command_t *father)
+                           command_t *father)
 {
-	/* TODO: Execute cmd1 and cmd2 simultaneously. */
+    /* TODO: Execute cmd1 and cmd2 simultaneously. */
 
-	return 0; /* TODO: Replace with actual exit status. */
+    return 0; /* TODO: Replace with actual exit status. */
 }
 
 /**
  * Run commands by creating an anonymous pipe (cmd1 | cmd2).
  */
 static int run_on_pipe(command_t *cmd1, command_t *cmd2, int level,
-		command_t *father)
+                       command_t *father)
 {
-	/* TODO: Redirect the output of cmd1 to the input of cmd2. */
+    /* TODO: Redirect the output of cmd1 to the input of cmd2. */
 
-	return 0; /* TODO: Replace with actual exit status. */
+    return 0; /* TODO: Replace with actual exit status. */
 }
 
 /**
@@ -212,36 +261,46 @@ static int run_on_pipe(command_t *cmd1, command_t *cmd2, int level,
  */
 int parse_command(command_t *c, int level, command_t *father)
 {
-	/* TODO: sanity checks */
+    /* TODO: sanity checks */
 
     ++level; // increment level for future call
     int ret;
 
-	switch (c->op) {
-    case OP_NONE:
-        return parse_simple(c->scmd, level, father);
+    switch (c->op) {
+        case OP_NONE:
+            ret = parse_simple(c->scmd, level, father);
+            break;
 
-	case OP_SEQUENTIAL:
-        if (parse_command(c->cmd1, level, c) == SHELL_EXIT) return SHELL_EXIT;
-        return parse_command(c->cmd2, level, c);
+        case OP_SEQUENTIAL:
+            if (parse_command(c->cmd1, level, c) == SHELL_EXIT)
+                ret = SHELL_EXIT;
+            else ret = parse_command(c->cmd2, level, c);
+            break;
 
-	case OP_PARALLEL:
-        return run_in_parallel(c->cmd1, c->cmd2, level, c);
+        case OP_PARALLEL:
+            ret = run_in_parallel(c->cmd1, c->cmd2, level, c);
+            break;
 
-	case OP_CONDITIONAL_NZERO:
-        ret = parse_command(c->cmd1, level, c);
-		if (ret == 0) return parse_command(c->cmd2, level, c);
-		return ret;
+        case OP_CONDITIONAL_NZERO:
+            ret = parse_command(c->cmd1, level, c);
+            if (ret == 0)
+                ret = parse_command(c->cmd2, level, c);
+            break;
 
-	case OP_CONDITIONAL_ZERO:
-        ret = parse_command(c->cmd1, level, c);
-        if (ret != 0) return parse_command(c->cmd2, level, c);
-        return ret;
+        case OP_CONDITIONAL_ZERO:
+            ret = parse_command(c->cmd1, level, c);
+            if (ret != 0)
+                ret = parse_command(c->cmd2, level, c);
+            break;
 
-	case OP_PIPE:
-		return run_on_pipe(c->cmd1, c->cmd2, level, c);
+        case OP_PIPE:
+            ret = run_on_pipe(c->cmd1, c->cmd2, level, c);
+            break;
 
-	default:
-		return SHELL_EXIT;
-	}
+        default:
+            ret = SHELL_EXIT;
+            break;
+    }
+
+    return ret;
 }
